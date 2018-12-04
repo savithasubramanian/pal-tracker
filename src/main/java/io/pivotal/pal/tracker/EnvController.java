445e90967d8cc46cab7f5d04f56b44f148cafca2
raymond700620 <rfang@pivotal.io>
2018-12-04 14:08:06 +0530

    Pushing to Gitlab

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,47 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    public String vport;
+    public String vmemlimit;
+    public String vcfinstind;
+    public String vcfinstaddr;
+
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv()
+    {
+       Map<String,String> setEnv = new HashMap<>();
+
+       setEnv.put("PORT",vport);
+        setEnv.put("MEMORY_LIMIT",vmemlimit);
+        setEnv.put("CF_INSTANCE_INDEX",vcfinstind);
+        setEnv.put("CF_INSTANCE_ADDR",vcfinstaddr);
+
+        System.out.println("PORT => " + vport);
+
+        return setEnv;
+
+    }
+
+    public EnvController(@Value("${PORT:8025}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String Ml,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}") String Cii,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}") String Cia)
+    {
+
+this.vport = port;
+this.vmemlimit=Ml;
+this.vcfinstind=Cii;
+this.vcfinstaddr=Cia;
+
+    }
+}
